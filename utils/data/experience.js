79037e5e8311984f9d9969a7281c74d869fc4c09
nildoria <sabbir.ahmed87@gmail.com>
2026-05-13 17:59:45 +0600

    content: update experience durations and Frontend Web Engineer title

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -3,18 +3,18 @@ export const experiences = [
     id: 1,
     title: 'WordPress Developer',
     company: "Pixiefy",
-    duration: "(2022 - Present)"
+    duration: "(2015 - Present)"
   },
   {
     id: 2,
     title: "Elementor & WooCommerce Developer",
     company: "Freelance / Remote Clients",
-    duration: "(2020 - Present)"
+    duration: "(2019 - Present)"
   },
   {
     id: 3,
-    title: "Frontend Web Developer",
+    title: "Frontend Web Engineer",
     company: "Self-Employed & Agency Projects",
-    duration: "(2019 - Present)"
+    duration: "(2021 - Present)"
   }
 ]
